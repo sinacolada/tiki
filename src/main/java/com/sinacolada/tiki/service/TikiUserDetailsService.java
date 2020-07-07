8fecbd091cfd3d5e7b654ebb90579ddd5d9228a8
Sina Soltanieh <sinasoltanieh@gmail.com>
2020-07-07 05:35:06 -0400

    some more work, dao -> jpa abst

--- src/main/java/com/sinacolada/tiki/service/TikiUserDetailsService.java
+++ src/main/java/com/sinacolada/tiki/service/TikiUserDetailsService.java
@@ -8,7 +8,6 @@ public class TikiUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
         return null;
     }
 
